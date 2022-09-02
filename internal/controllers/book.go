fd2ea1e92715f03e8744f40433cb31db3afb406a
Evan Sosenko <razorx@evansosenko.com>
2022-09-02 11:40:11 -0500

    Rename GetBookById

--- internal/controllers/book.go
+++ internal/controllers/book.go
@@ -4,6 +4,6 @@ import "net/http"
 
 func CreateBook(w http.ResponseWriter, r *http.Request)  {}
 func GetBooks(w http.ResponseWriter, r *http.Request)    {}
-func GetBookById(w http.ResponseWriter, r *http.Request) {}
+func GetBookByID(w http.ResponseWriter, r *http.Request) {}
 func UpdateBook(w http.ResponseWriter, r *http.Request)  {}
 func DeleteBook(w http.ResponseWriter, r *http.Request)  {}
--- internal/routes/book.go
+++ internal/routes/book.go
@@ -8,7 +8,7 @@ import (
 func RegisterBookRoutes(router *mux.Router) {
 	router.HandleFunc("/book", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book", controllers.GetBooks).Methods("GET")
-	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookByID).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
 	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 }
